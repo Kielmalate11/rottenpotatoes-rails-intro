0604d82f4a35bbcb7c99521a610f21bfe6090613
km3851 Kiel Malate <km3851@columbia.edu>
2025-10-04 20:26:55 +0000

    Part 3: remember filters & sorting using session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,41 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    @ratings_to_show = params[:ratings]&.keys || @all_ratings
-    @sort_by = params[:sort_by].presence
+    
+    ratings_param = params[:ratings]
+    sort_param = params[:sort_by]
+
+    if (ratings_param.blank? && session[:ratings].present?) ||
+       (sort_param.blank? && session[:sort_by].present?)
+     redirect_to movies_path(
+       ratings: (ratings_param || session[:ratings]),
+       sort_by: (sort_param || session[:sort_by])
+     ) and return
+    end
+
+
+    @ratings_to_show = 
+      if ratings_param.present?
+        ratings_param.keys
+      elsif session[:ratings].present?
+        session[:ratings].keys
+      else
+       @all_ratings
+      end
+
+    @sort_by = 
+      if sort_param.present?
+        sort_param
+      elsif session[:sort_by].present?
+        session[:sort_by]
+      else
+        nil
+      end
     @sort_by = nil unless %w[title release_date].include?(@sort_by)
+
+    session[:ratings] = @ratings_to_show.index_with { '1' }
+    session[:sort_by] = @sort_by
+
     @movies = Movie.with_ratings(@ratings_to_show)
     @movies = @movies.order(@sort_by) if @sort_by.present?
   end
